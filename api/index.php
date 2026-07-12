065f215424c93711858863d769661d2e6c455e1d
Syafiq Ikhsan <syfqiksn@gmail.com>
2026-07-12 15:51:01 +0700

    plis

--- api/index.php
+++ api/index.php
@@ -6,7 +6,50 @@ ini_set('log_errors', '1');
 
 define('LARAVEL_START', microtime(true));
 
+if (getenv('VERCEL')) {
+    $tmpDir = sys_get_temp_dir();
+    $storagePath = $tmpDir . '/laravel-storage';
+
+    putenv('APP_CONFIG_CACHE=' . $tmpDir . '/config.php');
+    putenv('APP_EVENTS_CACHE=' . $tmpDir . '/events.php');
+    putenv('APP_PACKAGES_CACHE=' . $tmpDir . '/packages.php');
+    putenv('APP_ROUTES_CACHE=' . $tmpDir . '/routes.php');
+    putenv('APP_SERVICES_CACHE=' . $tmpDir . '/services.php');
+    putenv('VIEW_COMPILED_PATH=' . $storagePath . '/framework/views');
+
+    $dbPath = $tmpDir . '/database.sqlite';
+    if (!file_exists($dbPath)) {
+        touch($dbPath);
+    }
+    putenv('DB_DATABASE=' . $dbPath);
+
+    $dirs = [
+        'app/public',
+        'framework/cache/data',
+        'framework/sessions',
+        'framework/testing',
+        'framework/views',
+        'logs',
+    ];
+
+    foreach ($dirs as $dir) {
+        $path = $storagePath . '/' . $dir;
+        if (!is_dir($path)) {
+            mkdir($path, 0755, true);
+        }
+    }
+
+    $publicStorage = $storagePath . '/app/public';
+    if (!is_dir($publicStorage)) {
+        mkdir($publicStorage, 0755, true);
+    }
+}
+
 require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
+
+if (isset($storagePath)) {
+    $app->useStoragePath($storagePath);
+}
 
 echo "Laravel booted successfully\n";
