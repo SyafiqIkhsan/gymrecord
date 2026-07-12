2cd90916c600cb3e39ec68b3b698a8c1f9de9c79
Syafiq Ikhsan <syfqiksn@gmail.com>
2026-07-12 12:13:06 +0700

    fix lagi lagi lagi

--- api/index.php
+++ api/index.php
@@ -4,8 +4,9 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 ini_set('log_errors', '1');
 
-echo 'VERCEL=' . var_export(getenv('VERCEL'), true) . "\n";
-echo 'VERCEL_ENV=' . var_export(getenv('VERCEL_ENV'), true) . "\n";
-echo 'PHP_VERSION=' . PHP_VERSION . "\n";
-echo 'HAS_VENDOR=' . (file_exists(__DIR__ . '/../vendor/autoload.php') ? 'yes' : 'no') . "\n";
-echo 'DOC_ROOT=' . ($_SERVER['DOCUMENT_ROOT'] ?? 'none') . "\n";
+define('LARAVEL_START', microtime(true));
+
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+echo "Laravel booted successfully\n";
